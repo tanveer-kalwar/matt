ef4bed413ae4a9df682f5b51f3887d58d6917f79
Mansoor <117315744+tanveer-kalwar@users.noreply.github.com>
2026-02-27 07:14:48 +0800

    Update __init__.py

--- mat_diff/__init__.py
+++ mat_diff/__init__.py
@@ -6,7 +6,7 @@ Geometry-Guided Imbalanced Multi-Class Classification
 __version__ = "1.0.0"
 
 from .fisher import FisherInformationEstimator
-from .geodesic_attention import GeodesicAttentionBlock
+from .geodesic_attention import GeodesicAttentionBlock, StandardAttentionBlock
 from .spectral_scheduler import SpectralCurriculumScheduler
 from .riemannian_privacy import RiemannianPrivacyFilter
 from .manifold_diffusion import MATDiffPipeline
@@ -15,8 +15,10 @@ from .denoiser import MATDiffDenoiser
 __all__ = [
     "FisherInformationEstimator",
     "GeodesicAttentionBlock",
+    "StandardAttentionBlock",
     "SpectralCurriculumScheduler",
     "RiemannianPrivacyFilter",
     "MATDiffPipeline",
     "MATDiffDenoiser",
 ]
+
